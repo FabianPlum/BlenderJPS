286809b4802c3306a27f2f8ed3e926fbca07e5e3
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2026-03-23 11:07:30 +0100

    don't zip too much unnecessary files

--- zipit.sh
+++ zipit.sh
@@ -4,6 +4,11 @@ set -euo pipefail
 zip -r BlenderJPS.zip blender_jps \
     -x "blender_jps/deps/*" \
     -x "blender_jps/tests/*" \
+    -x "blender_jps/examples/*" \
     -x "blender_jps/__pycache__/*" \
     -x "blender_jps/**/__pycache__/*" \
-    -x "*.pyc"
+    -x "*.pyc" \
+    -x "*~" \
+    -x "*.~undo-tree~" \
+    -x "*.sqlite" \
+    -x "*.DS_Store"
